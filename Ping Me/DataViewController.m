b24535c80a9a7ff3553d7b53cb25625fc4e84bef
Michael Poon <michael.l.poon@gmail.com>
2014-10-18 15:42:22 -0700

    initial commit

---
+++ Ping Me/DataViewController.m
@@ -0,0 +1,32 @@
+//
+//  DataViewController.m
+//  Ping Me
+//
+//  Created by Michael L Poon on 10/18/14.
+//  Copyright (c) 2014 Michael L Poon. All rights reserved.
+//
+
+#import "DataViewController.h"
+
+@interface DataViewController ()
+
+@end
+
+@implementation DataViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.dataLabel.text = [self.dataObject description];
+}
+
+@end
---
+++ Ping Me/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Ping Me
+//
+//  Created by Michael L Poon on 10/18/14.
+//  Copyright (c) 2014 Michael L Poon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
